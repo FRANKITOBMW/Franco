c35419c914eafb093a6eae2054ed5865d30df6e1
Franco Aleksic <you@example.com>
2025-04-23 10:29:43 -0300

    alec

--- steam.js
+++ steam.js
@@ -1,15 +1,9 @@
-document.getElementById("calcularBtn").addEventListener("click", calcularPrecio);
-
-function calcularPrecio() {
-  const dolarOficial = 900;
-
+function calcular1() {
+  const dolarOficial = 1000;
   const precioUSD = parseFloat(document.getElementById("precioUsd").value);
-
   const precioBase = precioUSD * dolarOficial;
   const impuestoGanancias = precioBase * 0.30;
   const impuestoIVA = precioBase * 0.21;
   const precioFinal = precioBase + impuestoGanancias + impuestoIVA;
-
-  document.getElementById("resultado").innerText =
-    `El precio final en pesos es: $${precioFinal.toFixed(2)}`;
+  document.getElementById("resultado").innerText =`El precio final en pesos es: ${precioFinal}`;
 }
